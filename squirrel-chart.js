4c418bdefda611690c44584921b1fdc1f4ddfab9
racheldaniell <44450960+racheldaniell@users.noreply.github.com>
2022-05-20 14:12:00 -0400

    update tooltip

--- squirrel-chart.js
+++ squirrel-chart.js
@@ -160,7 +160,7 @@
       .on("mouseover", function(event,d,i){
       tooltip
       .html(`<div>activity: ${d.activity}</div><div>sightings: ${d.count}</div>`)
-      .attr("class","tooltip-bar")
+      .style("class","tooltip-bar")
       .style("visibility", "visible")
       .style("opacity", .8)
       .style("background", tipColor)
@@ -169,9 +169,10 @@
           .attr("fill", hoverColor);
       })
       .on('mousemove', function (event) {
+        let pos = d3.mouse(this);
         tooltip
-          .style('top', event.pageY - 10 + 'px')
-          .style('left', event.pageX + 10 + 'px');
+          .style('top', pos[0] - 10 + 'px')
+          .style('left', pos[1] + 10 + 'px');
     })
     .on("mouseout", function(event, d){
       tooltip
